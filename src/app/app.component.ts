20ed6a6841ce77a92dd8f56c92268890542873ac
Arunava Das <64685513+arunava-webdev4u@users.noreply.github.com>
2025-03-12 01:10:53 +0530

    added modules folder and added home component (#1)
    * added modules folder and added home component
    
    * added video background, and some more content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./shared/components/navbar/navbar.component";
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { HomeComponent } from './modules/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
