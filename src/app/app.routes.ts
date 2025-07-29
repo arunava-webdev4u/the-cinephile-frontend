d8057553144fb1b7a6f1bee335cd1d1c44145120
Arunava Das <64685513+arunava-webdev4u@users.noreply.github.com>
2025-07-30 00:44:56 +0530

    logout works (#4)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { HomeComponent } from './modules/home/home.component'
 import { ListsComponent } from './modules/lists/lists.component'
 import { AddListComponent } from './modules/add-list/add-list.component'
 import { UsersComponent } from './modules/users/users.component';
+import { ProfileComponent } from './modules/profile/profile.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'lists', component: ListsComponent },
     { path: 'users', component: UsersComponent },
     { path: 'add-list', component: AddListComponent },
+    { path: 'profile', component: ProfileComponent },
     { path: 'auth', loadChildren: () => import("./modules/auth/auth.module").then(m => m.AuthModule) },
     { path: '**', redirectTo: '' }
 ];
