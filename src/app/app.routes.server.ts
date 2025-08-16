e8e253dc392c2abaf003bd649bb673427b84ebdc
Arunava Das <arunava52486@gmail.com>
2025-08-16 15:38:33 +0530

    disabled pre-render at root level

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'show/movie/**',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'show/tv/**',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
